d7e6c60494d1bd30968084fe713a5da9360fe7a8
jatin31195 <jatinsingh31185@gmail.com>
2024-04-07 02:33:04 +0530

    intial commit of frontend begin

---
+++ frontend/chatShat/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import MainContainer from './Components/MainContainer'
+function App() {
+
+  return (
+    <>
+      <div className="App">
+      <MainContainer/>
+      </div>
+    </>
+  )
+}
+
+export default App
