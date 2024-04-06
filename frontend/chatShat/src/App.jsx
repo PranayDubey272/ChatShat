8a97a4d1180ab9583aba9b3ada8a9c6786cf7914
Nitesh Kumar <nyrao0017@gmail.com>
2024-04-07 02:32:56 +0530

    Initial commit

---
+++ frontend/chatShat/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import MainContainer from "./Components/MainContainer.jsx";
+import Login from "./Components/Login.jsx"
+import './App.css'
+
+function App() {
+  return <div className="App">
+      <MainContainer/>
+      {/* <Login/> */}
+    </div>
+}
+
+export default App
