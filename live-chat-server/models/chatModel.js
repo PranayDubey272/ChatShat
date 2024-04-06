502053ec4b0633286c3f3fef9178aaf2c111008a
pranaydubey272 <pranaydubey272@gmail.com>
2024-04-06 22:10:17 +0530

    chatModel added

---
+++ live-chat-server/models/chatModel.js
@@ -0,0 +1,24 @@
+const mongoose  = require('mongoose');
+
+const chatModel =  mongoose.Schema({{
+    chatName : {type: 'string'},
+    isGroupChat : {type: 'boolean'},
+    users : [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref :'User'
+        }
+    ],
+    latestMessage : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'Message'
+    },
+    groupAdmin : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref :'User'
+    },
+});
+
+const Chat = mongoose.model("Chat",chatModel);
+
+module.exports = Chat;
