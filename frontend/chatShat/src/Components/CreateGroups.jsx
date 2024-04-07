c10a7b5accf9e04e3412a69fef1634c5ca18aaa9
Nitesh Kumar <nyrao0017@gmail.com>
2024-04-07 15:31:50 +0530

    login and Create groups

---
+++ frontend/chatShat/src/Components/CreateGroups.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {IconButton} from "@mui/material";
+import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
+
+function CreateGroups(){
+    return (<div className="creategroup-container">
+            <input type="text" placeholder="Enter Group Name" className="search-box" />
+            <IconButton>
+                <DoneOutlineIcon />
+            </IconButton>
+        </div>);
+}
+
+export default CreateGroups
