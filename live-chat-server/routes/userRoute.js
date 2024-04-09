27a862a402bef7056243f76bd74379038b39750d
pranaydubey272 <pranaydubey272@gmail.com>
2024-04-09 19:38:42 +0530

    userRoute-fixed

--- live-chat-server/routes/userRoute.js
+++ live-chat-server/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 // const app = express();
-const Router = express.Router();
 const { loginController,registerController } = require("../controllers/userContoller");
+// const {protect} = require("../middleware/authMiddleware");
+const Router = express.Router();
 Router.post('/login',loginController);
 Router.post('/register',registerController);
-
+// Router.get('/fetchUsers',protect,fetchAllUsersController);
 module.exports = Router;
