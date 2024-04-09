84b70cac22eb7a31c344e26934645ba5f83b1dba
pranaydubey272 <pranaydubey272@gmail.com>
2024-04-10 00:02:37 +0530

    self-add-contoller-created

--- live-chat-server/controllers/chatControllers.js
+++ live-chat-server/controllers/chatControllers.js
@@ -121,7 +121,28 @@ const groupExit = asyncHandler(async (req,res)=>{
   }
 });
 
+const addSelfToGroup = asyncHandler(async (req,res)=>{
+  const {chatId,userId} = req.body;
 
+  const added = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $push: { users: userId},
+    },
+    {
+      new: true,
+    }
+  ).populate("users", "-password")
+  .populate("groupAdmin","-password");
+   
+  if(!added){
+    req.send(400);
+    throw new Error("Chat Not found");
+  }
+  else{
+    res.json(added);
+  }
+})
 
 module.exports ={
     accessChat,
