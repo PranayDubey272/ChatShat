40197571db955dda32a5762e0b233de8d897328d
pranaydubey272 <pranaydubey272@gmail.com>
2024-04-09 19:34:30 +0530

    routeFixed

--- live-chat-server/index.js
+++ live-chat-server/index.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const {default:mongoose} = require("mongoose");
 const app = express();
+const userRoutes = require("./routes/userRoute");
+
 dotenv.config();
+app.use(express.json());
+
+// console.log(mongoose.connect(process.env.mongoURI));
+
+const connectDB  = async()=>{
+    try {
+        await mongoose.connect(process.env.mongoURI);
+        console.log(`Connected to MongoDB ${mongoose.connection.host}`);
+    } catch (error) {
+        console.log("Error in connecting ",error);
+        process.exit(1);
+    }
+}
+connectDB();
+
 app.get("/", (req, res)=>{
     res.send("API is running..");
 });
-const PORT = process.env.PORT || 3000;
+app.use("/user",userRoutes);
+
+const PORT = process.env.PORT;
 app.listen(3000,console.log("listening on port 3000"));
