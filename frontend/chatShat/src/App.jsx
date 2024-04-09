2ad75618e343c7ba56c0baf25dc54747aa9d44ad
Nitesh Kumar <nyrao0017@gmail.com>
2024-04-10 00:36:21 +0530

    add routing in app.jsx

--- frontend/chatShat/src/App.jsx
+++ frontend/chatShat/src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react';
-import MainContainer from "./Components/MainContainer.jsx";
-import Login from "./Components/Login.jsx"
+import { useState } from 'react'
 import './App.css'
-
+import MainContainer from './Components/MainContainer'
+import Login from './Components/Login'
+import {Routes,Route} from "react-router-dom";
+import Welcome from './Components/Welcome';
+import ChatArea from './Components/ChatArea';
+import Users from './Components/Users';
+import CreateGroups from './Components/CreateGroups';
+import Groups from './Components/Groups';
 function App() {
-  return <div className="App">
-      {/* <MainContainer/> */}
+
+  return (
+    <>
+      <div className="App">
+{/*         
       <Login/>
-    </div>
+      <MainContainer/> */}
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path='app' element={<MainContainer/>}>
+          <Route path='welcome' element={<Welcome/>}></Route>
+          <Route path='chat' element={<ChatArea/>}></Route>
+          <Route path='user' element={<Users/>}></Route>
+          <Route path='groups' element={<Groups/>}></Route>
+
+          <Route path='create-groups' element={<CreateGroups/>}></Route>
+          </Route>
+      </Routes>
+      </div>
+    </>
+  )
 }
 
 export default App
