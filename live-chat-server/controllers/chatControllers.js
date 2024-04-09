6f555db3eb04eed15921043b1947bb6dfb1bd339
pranaydubey272 <pranaydubey272@gmail.com>
2024-04-10 00:00:03 +0530

    fetch-chat-controller-created

--- live-chat-server/controllers/chatControllers.js
+++ live-chat-server/controllers/chatControllers.js
@@ -45,6 +45,26 @@ const accessChat = asyncHandler(async (req,res)=>{
   }
 
 });
+const fetchChats = asyncHandler(async (req,res)=>()=>{
+    try{
+      Chat.find({users: {$element :{$eq: req.user.id}}})
+      .populate("users","-password")
+      .populate("groupAdmin","-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) =>{
+        results = await User.populate(results,{
+          path: "latestMessage.sender",
+          select: "name email",
+        });
+        res.status(200).send(results);
+      });
+    } catch(error){
+      res.status(400);
+      throw new Error(error.message);
+    }
+});
+
 
 
 module.exports ={
