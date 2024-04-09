8321cbee3cd2e83208f2ea4f5d086e10fd05bd64
pranaydubey272 <pranaydubey272@gmail.com>
2024-04-10 02:42:05 +0530

    cors-for-frontend-added

--- live-chat-server/index.js
+++ live-chat-server/index.js
@@ -3,10 +3,13 @@ const dotenv = require("dotenv");
 const {default:mongoose} = require("mongoose");
 const app = express();
 const userRoutes = require("./routes/userRoute");
-
+const messageRoutes = require("./routes/messageRoutes");
+const chatRoute = require("./routes/chatRoute");
+const { notfound, errorhandler } = require("./middleware/errorMiddleware");
+const cors = require('cors');
 dotenv.config();
 app.use(express.json());
-
+app.use(cors());
 // console.log(mongoose.connect(process.env.mongoURI));
 
 const connectDB  = async()=>{
@@ -23,7 +26,12 @@ connectDB();
 app.get("/", (req, res)=>{
     res.send("API is running..");
 });
-app.use("/user",userRoutes);
 
+app.use("/user",userRoutes);
+app.use("/chat",chatRoute);
+app.use("/message",messageRoutes);
+
+app.use(notfound);
+app.use(errorhandler);
 const PORT = process.env.PORT;
 app.listen(3000,console.log("listening on port 3000"));
