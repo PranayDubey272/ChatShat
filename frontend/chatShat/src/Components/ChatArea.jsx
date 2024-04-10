b41d0a257624118f3e1acb4969b7651a8f3d5a0d
Nitesh Kumar <nyrao0017@gmail.com>
2024-04-11 02:36:14 +0530

    ChatArea dark theme added

--- frontend/chatShat/src/Components/ChatArea.jsx
+++ frontend/chatShat/src/Components/ChatArea.jsx
@@ -6,7 +6,11 @@ import SendIcon from '@mui/icons-material/Send';
 import CloseIcon from '@mui/icons-material/Close';
 import MessageOthers from './MessageOthers';
 import MessageSelf from './MessageSelf';
+import { useSelector } from 'react-redux';
 function ChatArea() {
+
+  const lightTheme = useSelector((state) => state.themeKey);
+
   const [conversations,setConversation]=useState([
     {
       name:"Test#1",
@@ -26,24 +30,24 @@ function ChatArea() {
   ]);
   var props=conversations[0];
   return (
-    <div className='chatArea-container'>
-      <div className="chatArea-header">
-        <p className="con-icon">{props.name[0]}</p>
-        <div className="header-text">
-          <p className='con-title'>{props.name}</p>
-          <p className='con-timeStamp'>{props.timeStamp}</p>
+    <div className={'chatArea-container' + (lightTheme ? "" : " dark")}>
+      <div className={"chatArea-header" + (lightTheme ? "" : " dark")}>
+        <p className={"con-icon" + (lightTheme ? "" : " dark")}>{props.name[0]}</p>
+        <div className={"header-text" + (lightTheme ? "" : " dark")}>
+          <p className={'con-title' + (lightTheme ? "" : " dark")}>{props.name}</p>
+          <p className={'con-timeStamp' + (lightTheme ? "" : " dark")}>{props.timeStamp}</p>
         </div>
         <div style={{ marginLeft: 'auto' }}>
         <IconButton>
           <CloseIcon className='close'/>
         </IconButton></div>
       </div>
-      <div className="messages-container">
+      <div className={"messages-container" + (lightTheme ? "" : " dark")}>
         <MessageOthers/>
         <MessageSelf/>
       </div>
-      <div className="text-input-area">        
-      <input placeholder='Type a Message' className='search-box'/>
+      <div className={"text-input-area" + (lightTheme ? "" : " dark")}>        
+      <input placeholder='Type a Message' className={'search-box' + (lightTheme ? "" : " dark")}/>
       <SendIcon/>
 </div>
     </div>
