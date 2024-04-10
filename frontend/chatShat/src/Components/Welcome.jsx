1c3a55410b6d7a96bbfbd9a7cd902640aacc207a
Nitesh Kumar <nyrao0017@gmail.com>
2024-04-11 03:00:09 +0530

    welcome page dark theme added

--- frontend/chatShat/src/Components/Welcome.jsx
+++ frontend/chatShat/src/Components/Welcome.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import logo from "/logo.png";
+import { useSelector } from "react-redux";
 
 function Welcome(){
+
+    const lightTheme = useSelector((state) => state.themeKey);
+
     return (
-    <div className="welcome-container">
-        <img src={logo} alt="Logo" className="welcome-logo" />
+    <div className={"welcome-container" + (lightTheme ? "" : " dark")}>
+        <img src={logo} alt="Logo" className={"welcome-logo" + (lightTheme ? "" : " dark")} />
         <p>Come and chat with us HABIBI</p>
         
     </div>);
