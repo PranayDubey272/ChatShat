ddd12ada699d36c48ca484de6b2864282b6ee8b8
jatin31195 <jatinsingh31185@gmail.com>
2024-04-12 17:25:11 +0530

    routing added in login

---
+++ frontend/chatShat/src/Components/Login.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "/logo.png";
+import { Button, TextField } from "@mui/material";
+
+function Login(){
+    return (
+        <div className="login-container">
+            <div className="image-container">
+                <img src={logo} alt="Logo" className="welcome-logo" />
+            </div>
+            <div className="login-box">
+                <p className="login-text">Login to your Account</p>
+                <TextField 
+                    id="standard-basic" 
+                    label="Enter User Name" 
+                    variant="outlined" 
+                />
+                <TextField 
+                    id="outlined-password-input" 
+                    label="Enter Password" 
+                    type="password"
+                    autoComplete="current-password" 
+                    variant="outlined" 
+                />
+                <Button variant="outlined">Log In</Button>
+            </div>
+        </div>    
+        );
+        
+}
+
+export default Login
