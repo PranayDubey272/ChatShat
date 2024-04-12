f6f762b5ed661c0ddb5980361466d299e2c4f04b
jatin31195 <jatinsingh31185@gmail.com>
2024-04-12 17:26:18 +0530

    group and user seprated

--- frontend/chatShat/src/Components/MessageOthers.jsx
+++ frontend/chatShat/src/Components/MessageOthers.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function MessageOthers() {
+
+  const lightTheme = useSelector((state) => state.themeKey);
+
   var props1={name:"RandomUser",message:"This is a sample message"}
   return (
-    <div className='other-message-container'>
-      <div className="conversations-container">
-        <p className='con-icon'>{props1.name[0]}</p>
-        <div className="other-text-content">
-        <p className="con-title">{props1.name}</p>
-      <p className='con-lastMessage'>{props1.message}</p>
-      <p className='con-timeStamp'>12:00am</p>
+    <div className={'other-message-container' + (lightTheme ? "" : " dark")}>
+      <div className={"conversations-container" + (lightTheme ? "" : " dark")}>
+        <p className={'con-icon' + (lightTheme ? "" : " dark")}>{props1.name[0]}</p>
+        <div className={"other-text-content" + (lightTheme ? "" : " dark")}>
+        <p className={"con-title" + (lightTheme ? "" : " dark")}>{props1.name}</p>
+      <p className={'con-lastMessage' + (lightTheme ? "" : " dark")}>{props1.message}</p>
+      <p className={'con-timeStamp' + (lightTheme ? "" : " dark")}>12:00am</p>
         </div>
       </div>
     </div>
