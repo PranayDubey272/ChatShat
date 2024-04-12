476111992739d183d8ea8ea76455107fe7d44844
Nitesh Kumar <nyrao0017@gmail.com>
2024-04-12 19:59:03 +0530

    welcome page dark theme enables

--- frontend/chatShat/src/Components/Welcome.jsx
+++ frontend/chatShat/src/Components/Welcome.jsx
@@ -8,9 +8,9 @@ function Welcome(){
     const lightTheme = useSelector((state) => state.themeKey);
 
     return (
-    <div className={"welcome-container" + (lightTheme ? "" : " dark")}>
-        <img src={lightTheme ? lightLogo : darkLogo} alt="Logo" className={"welcome-logo" + (lightTheme ? "" : " dark")} />
-        <p>Come and chat with us HABIBI</p>
+    <div className={"welcome-container" + (lightTheme ? "" : " midDark")}>
+        <img src={lightTheme ? lightLogo : darkLogo} alt="Logo" className={"welcome-logo" + (lightTheme ? "" : " midDark")} />
+        <p className={lightTheme ? "" : " midDark"}>Come and chat with us HABIBI</p>
         
     </div>);
 }
