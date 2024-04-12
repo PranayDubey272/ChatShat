7c3c7d280127f027531974bb02f4a3127b273ad8
jatin31195 <jatinsingh31185@gmail.com>
2024-04-12 17:26:44 +0530

    css for flex side added

--- frontend/chatShat/src/Components/MessageSelf.jsx
+++ frontend/chatShat/src/Components/MessageSelf.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function MessageSelf() {
+
+  const lightTheme = useSelector((state) => state.themeKey);
+
   var props2={name:"You",message:"This is me"}
   return (
-    <div className='self-message-container'>
-      <div className="messageBox">
+    <div className={'self-message-container' + (lightTheme ? "" : " dark")}>
+      <div className={"messageBox" + (lightTheme ? "" : " dark")}>
       <p>{props2.message}</p>
-      <p className='self-timeStamp'>12:00am</p>
+      <p className={'self-timeStamp' + (lightTheme ? "" : " dark")}>12:00am</p>
       </div>
     </div>
   )
