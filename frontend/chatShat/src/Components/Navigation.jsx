249dff0e2cf145b19a386d3dab905e133e6b6a89
jatin31195 <jatinsingh31185@gmail.com>
2024-04-12 17:27:43 +0530

    span removed from button

--- frontend/chatShat/src/Components/Navigation.jsx
+++ frontend/chatShat/src/Components/Navigation.jsx
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
-import { IoIosAddCircle } from "react-icons/io";
-import { IoPersonCircleSharp, IoPersonAddSharp } from "react-icons/io5";
-import { MdGroupAdd, MdNightlight } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 import './Navigation.css';
+import { MdGroupAdd } from "react-icons/md";
+import { IoIosAddCircle } from "react-icons/io";
+import { IoPersonCircleSharp } from "react-icons/io5";
+import { MdNightlight } from "react-icons/md";
+import { IoSunnySharp } from "react-icons/io5";
+import { IoPersonAddSharp } from "react-icons/io5";
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'; // Import useSelector
+import { toggleTheme } from '../Features/themeSlice';
 
 function Navigation() {
-  const [activeIndex, setActiveIndex] = useState(0);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const lightTheme = useSelector(state => state.themeKey); // Access themeKey from Redux state
+
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const handleHover = (index) => {
     setActiveIndex(index);
@@ -19,38 +27,35 @@ function Navigation() {
 
   return (
     <div className='body'>
-      <div className='navigation'>
+      <div className={"navigation" + (lightTheme ? "" : " dark")}>
         <ul>
           <li className={`list ${activeIndex === 0 ? 'active' : ''}`} onMouseEnter={() => handleHover(0)}>
             <button onClick={() => handleNavigation("/")}>
-              <span className="icon"><IoPersonCircleSharp /></span>
+              <IoPersonCircleSharp className="icon" />
             </button>
           </li>
           <li className={`list ${activeIndex === 1 ? 'active' : ''}`} onMouseEnter={() => handleHover(1)}>
             <button onClick={() => handleNavigation("/app/user")}>
-              <span className="icon"><IoPersonAddSharp /></span>
+              <IoPersonAddSharp className="icon" />
             </button>
           </li>
           <li className={`list ${activeIndex === 2 ? 'active' : ''}`} onMouseEnter={() => handleHover(2)}>
             <button onClick={() => handleNavigation("/app/groups")}>
-              <span className="icon"><MdGroupAdd /></span>
+              <MdGroupAdd className="icon" />
             </button>
           </li>
           <li className={`list ${activeIndex === 3 ? 'active' : ''}`} onMouseEnter={() => handleHover(3)}>
             <button onClick={() => handleNavigation("/app/create-groups")}>
-              <span className="icon"><IoIosAddCircle /></span>
+              <IoIosAddCircle className="icon" />
             </button>
           </li>
           <li className={`list ${activeIndex === 4 ? 'active' : ''}`} onMouseEnter={() => handleHover(4)}>
-            <button onClick={() => handleNavigation("#")}>
-              <span className="icon"><MdNightlight /></span>
+            <button onClick={() => dispatch(toggleTheme())} className={`theme-toggle-button ${lightTheme ? "" : "dark"}`}>
+              {lightTheme ? <MdNightlight className="icon" /> : <IoSunnySharp className="icon" />}
             </button>
           </li>
-
-        {/* Indicator element to highlight active tab */}
-        <div className='indicator' style={{ transform: `translateX(calc(${activeIndex} * 100%))` }}></div>
+          <div className='indicator' style={{ transform: `translateX(calc(${activeIndex} * 100%))` }}></div>
         </ul>
-
       </div>
     </div>
   );
