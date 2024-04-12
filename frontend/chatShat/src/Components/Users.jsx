4ac50bc8fe0beb23f1e44e5d0baa557f33f1d185
jatin31195 <jatinsingh31185@gmail.com>
2024-04-12 17:28:43 +0530

    user and group seprated

---
+++ frontend/chatShat/src/Components/Users.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import "./myStyles.css";
+import SearchIcon from "@mui/icons-material/Search";
+import { IconButton } from "@mui/material";
+import logo from "/logo.png";
+import { useSelector } from "react-redux";
+
+function Users(){
+
+    const lightTheme = useSelector((state) => state.themeKey);
+
+    return (
+    <div className={"list-container" + (lightTheme ? "" : " dark")}>
+        <div className={"ug-header" + (lightTheme ? "" : " dark")}>
+            <img src={logo} alt="logo" style={{height: "3rem",
+            width: "5rem", marginLeft: "10px"}}/>
+            <p className={"ug-title" + (lightTheme ? "" : " dark")}>Online Users</p>
+        </div>
+        <div className={"sb-search" + (lightTheme ? "" : " dark")}>
+            <IconButton>
+                <SearchIcon/>
+            </IconButton>
+            <input type="text" placeholder="Search" className={"search-box" + (lightTheme ? "" : " dark")} />
+        </div>
+
+        <div className={"ug-list" + (lightTheme ? "" : " dark")}>
+
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+            <div className={"list-item" + (lightTheme ? "" : " dark")}>
+                <p className={"con-icon" + (lightTheme ? "" : " dark")}>T</p>
+                <p className={"con-title" + (lightTheme ? "" : " dark")}>Test User</p>
+            </div>
+        </div>
+        
+    </div>
+    );
+}
+
+export default Users;
